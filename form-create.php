36711162a3bc068645573ad34b58185f1cf44c49
TheNuee <43197392+TheNuee@users.noreply.github.com>
2019-03-18 11:22:48 +0700

    Add files via upload

---
+++ form-create.php
@@ -0,0 +1,16 @@
+
+<html>
+    <head>
+        <title>Tambah Data Dosen</title>
+    </head>
+
+    <body>
+    <form action="create.php" method="POST">
+
+        <h3>Nama dosen :<input type="text" name="nama_dosen"></h3>
+        <h3>No. Telepon :<input type="text" name="telp"></h3>
+        <input type="submit" name="btnSimpan" value="Simpan">
+        </form>
+    </body>
+    
+</html>
